214309dcf26fe31615ebe5cdc06693eaf9cb7ee1
j206 <35281996+j206@users.noreply.github.com>
2022-10-20 01:47:40 -0700

    Bug squashing attempts

--- src/Tiles.jsx
+++ src/Tiles.jsx
@@ -1,22 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import anime from "animejs";
 import "./Tiles.css";
 
 function Tiles() {
   // super busted
   const [gridsize, setGridsize] = useState({ columns: 0, rows: 0, total: 1 });
 
-  // handleStagger goes here
+  useEffect(() => {
+    getGridSize();
+    window.addEventListener("resize", getGridSize);
+  });
+
+  const handleStagger = (i) => {
+    const { columns, rows } = gridsize;
+    const el = i.target.id;
+
+    anime({
+      targets: ".grid-item",
+      backgroundColor: randomColor(),
+      delay: anime.stagger(50, { grid: [columns, rows], from: el }),
+    });
+  };
 
   const getGridSize = () => {
     const columns = Math.floor(document.body.clientWidth / 50);
     const rows = Math.floor(document.body.clientHeight / 50);
 
-    setGridsize({ columns, rows, total: rows * columns });
+    // setGridsize({ columns, rows, total: rows * columns });
     console.log(gridsize);
+    
   };
-  getGridSize();
 
-  return <div className="Tiles"></div>;
+  const { total, columns, rows } = gridsize;
+
+  return (
+    <div id="grid">
+      {[...Array(total)].map((x, i) => (
+        <div
+          className="grid-item"
+          id={i}
+          onClick={(i) => handleStagger(i)}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Tiles;
